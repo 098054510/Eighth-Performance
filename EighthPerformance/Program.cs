9e1c3b7742831d0c97aee3c771ab115ebc3c9138
Eduardo de Souza <eduardohsfreitas86@gmail.com>
2018-11-06 07:58:04 -0300

    Eighth Performance/Program made in C#

---
+++ EighthPerformance/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace EighthPerformance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N1, N2, soma;
+            string[] vet = Console.ReadLine().Split(' ');
+
+            N1 = int.Parse(vet[0]);
+            N2 = int.Parse(vet[1]);
+
+            soma = 0;
+
+            for (int i=N1; i<=N2; i++)
+            {
+                if(i % 2!= 0)
+                {
+                    soma = soma + i;
+                }
+            }
+
+            Console.WriteLine(soma);
+            Console.ReadLine();
+        }
+    }
+}
